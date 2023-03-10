3317b777766df860ddc0431c4365dea297c7c4a4
Samandar Bohodirov <samandarbohodirov@Samandars-Mac-mini.local>
2023-03-10 15:51:08 +0500

    charts

---
+++ src/views/line charts/Spline Chart.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CanvasJSReact from '../../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+ 
+class SplineChart extends Component {
+	render() {
+		const options = {
+			animationEnabled: true,
+			title:{
+				text: "Monthly Sales - 2017"
+			},
+			axisX: {
+				valueFormatString: "MMM"
+			},
+			axisY: {
+				title: "Sales (in USD)",
+				prefix: "$",
+				includeZero: false
+			},
+			data: [{
+				yValueFormatString: "$#,###",
+				xValueFormatString: "MMMM",
+				type: "spline",
+				dataPoints: [
+					{ x: 1, y: 64 },
+					{ x: new Date(2017, 1), y: 27980 },
+					{ x: new Date(2017, 2), y: 42800 },
+					{ x: new Date(2017, 3), y: 32400 },
+					{ x: new Date(2017, 4), y: 35260 },
+					{ x: new Date(2017, 5), y: 33900 },
+					{ x: new Date(2017, 6), y: 40000 },
+					{ x: new Date(2017, 7), y: 52500 },
+				]
+			}]
+		}
+		
+		return (
+		<div>
+			<h1>React Spline Chart</h1>
+			<CanvasJSChart options = {options} 
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+}
+
+export default SplineChart;                           
